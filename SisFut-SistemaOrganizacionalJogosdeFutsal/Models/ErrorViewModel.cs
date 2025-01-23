6c3d8e548c65d45fa9565af416acec29bd0475fe
Rafaela Ranieri Lassen <lassenrafaela@gmail.com>
2025-01-23 17:32:26 -0300

    Adicionar arquivos de projeto.

---
+++ SisFut-SistemaOrganizacionalJogosdeFutsal/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SisFut_SistemaOrganizacionalJogosdeFutsal.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
